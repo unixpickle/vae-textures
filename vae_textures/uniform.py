f7962f6dd28e750d63cffe438bfa32bc35eee0c4
Alex Nichol <unixpickle@gmail.com>
2021-09-08 09:14:28 -0400

    commit

--- vae_textures/uniform.py
+++ vae_textures/uniform.py
@@ -5,6 +5,14 @@ import jax.numpy as jnp
 @jax.jit
 def gauss_to_uniform(g: jnp.ndarray):
     """
-    Map normal random variable to uniform random variables.
+    Map normal random variables to uniform random variables.
     """
     return jax.scipy.stats.norm.cdf(g) * 2 - 1
+
+
+@jax.jit
+def uniform_to_gauss(g: jnp.ndarray):
+    """
+    Map uniform random variable to normal random variables.
+    """
+    return jax.scipy.special.ndtri(jnp.clip((g + 1) / 2, -0.00001, 0.99999))
