889bb394354060a487ccf02279a4227551172d54
Alex Nichol <unixpickle@gmail.com>
2021-09-26 16:59:42 -0400

    mesh-ray collisions

--- vae_textures/render.py
+++ vae_textures/render.py
@@ -1,20 +1,29 @@
+import jax
 import jax.numpy as jnp
 from typing import Tuple
 
 
-def _triangle_ray_collision(
-    t: jnp.ndarray, origin: jnp.ndarray, dir: jnp.ndarray
-) -> Tuple[jnp.ndarray, jnp.ndarray]:
+def mesh_ray_collision(mesh: jnp.ndarray, origin: jnp.ndarray, dir: jnp.ndarray):
     """
-    Shoot a ray from an origin point in a given direction and check if and
-    where it collides with the triangle t.
+    Shoot a ray from an origin point in a given direction and find the first
+    place it intersects a mesh, if anywhere.
 
-    :param t: a [3 x 3] array (rows are points, columns are X, Y, Z).
-    :param origin: a 1-D array, the ray origin point.
+    :param mesh: an [N x 3 x 3] triangle mesh.
+    :param origin: 1 1-D array, the ray origin point.
     :param dir: a 1-D array, the ray direction.
     :return: a tuple (collides, point), where collides is a 0-D boolean tensor
              and point is a 1-D coordinate for an intersection.
     """
+    collides, positions = jax.vmap(_triangle_ray_collision)(mesh, origin, dir)
+    distances = jnp.where(
+        collides, jnp.sum((positions - origin) ** 2, axis=-1), jnp.inf
+    )
+    return jnp.any(collides), positions[jnp.argmin(distances)]
+
+
+def _triangle_ray_collision(
+    t: jnp.ndarray, origin: jnp.ndarray, dir: jnp.ndarray
+) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
     # Adapted from https://github.com/unixpickle/model3d/blob/efe391aa6714a269aac3f09de525406a90fa51bc/model3d/primitives.go#L169
     normal = _triangle_normal(t)
     dir = _normalize(dir)
